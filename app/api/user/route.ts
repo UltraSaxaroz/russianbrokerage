8d18677d4711ffd406b875e71fe908ec3d1536f5
UltraSaxaroz <fonbidon@gmail.com>
2024-10-15 19:47:36 +0400

    😭

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -65,11 +65,11 @@ export async function PUT(request: NextRequest): Promise<NextResponse> {
     const db = client.db();
 
     try {
-        const {id, name, email, role} = await request.json();
+        const {_id, name, email, role} = await request.json();
 
         await db.collection('users').updateOne(
-            {_id: id},  // Проверяем принадлежность данных пользователю
-            {$set: {name, email, role}}
+            {_id: new ObjectId(_id)},  // Проверяем принадлежность данных пользователю
+            {$set: {_id, name, email, role}}
         );
 
         return NextResponse.json({message: 'Driver updated successfully'}, {status: 200});
