a161562d206b75eff5a86f202370fa21a551348e
UltraSaxaroz <fonbidon@gmail.com>
2024-10-15 18:51:26 +0400

    Revert "idk wtf is this"
    This reverts commit 42593342

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -3,6 +3,7 @@ import jwt, { JwtPayload } from 'jsonwebtoken';
 import { NextRequest, NextResponse } from 'next/server';
 import { ObjectId } from 'mongodb';
 
+// Получить всех пользователей с возможностью фильтрации по имени
 export async function GET(request: NextRequest): Promise<NextResponse> {
     const token = request.headers.get('authorization')?.split(' ')[1];
     if (!token) {
@@ -21,10 +22,15 @@ export async function GET(request: NextRequest): Promise<NextResponse> {
     }
 
     const client = await clientPromise;
-    const db = client.db();
+    const db = client.db(); // Убедитесь, что имя базы данных правильное
+
+    const { search } = request.nextUrl; // Получаем строку поиска из URL
 
     try {
-        const users = await db.collection('users').find({}).toArray();
+        // Поиск пользователей по имени
+        const query = search ? { name: { $regex: search, $options: 'i' } } : {};
+        const users = await db.collection('users').find(query).toArray();
+
         return NextResponse.json({ data: users.map(user => ({ _id: user._id.toString(), name: user.name, email: user.email, role: user.role })) }, { status: 200 });
     } catch (err) {
         console.error(err);
@@ -32,6 +38,7 @@ export async function GET(request: NextRequest): Promise<NextResponse> {
     }
 }
 
+// Добавьте новый метод для обновления пользователя
 export async function PATCH(request: NextRequest): Promise<NextResponse> {
     const token = request.headers.get('authorization')?.split(' ')[1];
     if (!token) {
@@ -50,14 +57,19 @@ export async function PATCH(request: NextRequest): Promise<NextResponse> {
     }
 
     const client = await clientPromise;
-    const db = client.db();
+    const db = client.db(); // Убедитесь, что имя базы данных правильное
 
-    const { _id, name, email, role } = await request.json();
+    const { _id, name, email, role } = await request.json(); // Теперь получаем роль
 
     try {
+        const updateData: { [key: string]: any } = { name, email };
+        if (role) {
+            updateData.role = role; // Добавляем роль в обновление, если она передана
+        }
+
         const result = await db.collection('users').updateOne(
             { _id: new ObjectId(_id) },
-            { $set: { name, email, role } }
+            { $set: updateData }
         );
 
         if (result.modifiedCount === 0) {
