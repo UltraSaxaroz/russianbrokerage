138297800dac1d94b9ec42a78ffa485cf228e304
UltraSaxaroz <fonbidon@gmail.com>
2024-10-15 21:14:04 +0400

    😭

--- middleware.ts
+++ middleware.ts
@@ -44,5 +44,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/admin/:path*', '/dashboard/:path*']
+    matcher: ['/admin/:path*', '/panel/:path*']
 }
